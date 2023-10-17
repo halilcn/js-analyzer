ccb622e98f6224da602224e328e9c0accabe29d6
Halil Can <54184509+halilcn@users.noreply.github.com>
2023-10-17 12:30:45 +0300

    ref: better regex rules

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -3,13 +3,13 @@ import { NamingConventionEnum } from '../types/index.js';
 export const DEFAULT_EXPECT_FILES = ['node_modules', 'dist', 'package.json', 'package-lock.json', '.git'];
 export const DEFAULT_EXPECT_TYPES = [''];
 
-// TODO: TEST_CASE_1 ---> PASCAL_CASE
 // @Created by ChatGPT
+// Their sorting is crucial in finding what conventional type of a text.
 export const NAMING_CONVENTIONS_REGEX = {
-  [NamingConventionEnum.KEBAB_CASE]: /^[a-z][a-z0-9-]*$/,
+  [NamingConventionEnum.KEBAB_CASE]: /^[a-z][a-z0-9-]*-[a-z0-9-]*$/,
   [NamingConventionEnum.PASCAL_CASE]: /^[A-Z][a-zA-Z0-9]*$/,
+  [NamingConventionEnum.SNAKE_CASE]: /^[a-z][a-z0-9-]*_[a-z0-9-]*$/,
   [NamingConventionEnum.CAMEL_CASE]: /^[a-z][a-zA-Z0-9]*$/,
-  [NamingConventionEnum.SNAKE_CASE]: /^[a-z][a-z0-9_]*$/,
   [NamingConventionEnum.UNDEFINED_CASE]: /^.*$/, // it should be at the end
 };
 
